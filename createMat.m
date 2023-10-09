277eb56cfc667c9f7a31389685aca608c1279e3f
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2023-10-08 22:50:38 -0600

    Commenting createMat.m
    Adding comments and header

--- createMat.m
+++ createMat.m
@@ -1,5 +1,10 @@
-% Create .mat File
-% Commented out since DEMs.mat exists in git
+% AUTHOR: Victoria Hurd
+% PROJECT: EVA Informatics
+% PURPOSE: Data formatting script. This script will take the two DEM files
+% and turn them into a Matlab-usable format. 
+
+% Read in DEM files
 elev = imread("SLDEM2015_512_30S_00S_315_360.JP2");
 slope = imread("SLDEM2015_512_SL_30S_00S_315_360.JP2");
-save("DEMs.mat");
+% Create .mat File
+save("./data/DEMs.mat");
