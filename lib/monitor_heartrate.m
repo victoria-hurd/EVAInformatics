eefe469baab8980e0650e2ebe09afef29f8b6833
Jessica Villalta <150885910+jeca1024@users.noreply.github.com>
2023-11-28 22:05:09 -0700

    Update monitor_heartrate.m
    updating heart rate criticality levels

--- lib/monitor_heartrate.m
+++ lib/monitor_heartrate.m
@@ -10,8 +10,12 @@
 function [heartrate_criticality_level] = monitor_heartrate(heartrate)
     %% The following is just an example. Please Complete
     heartrate_criticality_level = 1;
-    if heartrate > 140
-        heartrate_criticality_level = 2;
+    if heartrate < 70
+        heartrate_criticality_level = 1; %lower heart rate
+    elseif heartrate >= 70 && heartrate <= 170
+                heartrate_criticality_level = 2; %within the ranges of a target heartrate
+    else heartrate_criticality_level = 3; %approaching and achieving max HR (175 bpm for a 45 year old)
+    
     end
 
 end
